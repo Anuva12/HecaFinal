583873a44b84fb4420707839e3045396220f1527
Anuva12Ghos <anuva.ghosalkar@gmail.com>
2020-07-12 17:07:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/playing/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.playing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.secondphone5a)
+    }
+}
